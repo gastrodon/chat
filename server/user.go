427f34da0a873620549e4699b6feadeeb0132eb6
basswaver <dakoolstwunn@gmail.com>
2019-10-17 23:00:28 -0400

    PUT /user (update uname)

--- server/user.go
+++ server/user.go
@@ -6,13 +6,72 @@ import (
 	"encoding/json"
 	"errors"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
+func putHandleUser(response http.ResponseWriter, request *http.Request) {
+	var body []byte
+	var json_body struct{
+		Username string `json:"username"`
+	}
+
+	var response_map map[string]interface{}
+	var user models.User
+	var exists bool
+	var err error
+
+	defer request.Body.Close()
+
+	if len(request.URL.Query()["key"]) == 0 {
+		HandleHTTPErr(response, errors.New("no_key"), 401)
+	}
+
+	var key string = request.URL.Query()["key"][0]
+
+	user, exists, err = io.UserFromKey(key)
+
+	if err != nil {
+		HandleHTTPErr(response, err, 500)
+		return
+	}
+
+	if !exists {
+		HandleHTTPErr(response, errors.New("bad_key"), 401)
+		return
+	}
+
+
+	body, err = ioutil.ReadAll(request.Body)
+
+	if err != nil {
+		HandleHTTPErr(response, err, 500)
+		return
+	}
+
+	err = json.Unmarshal(body, &json_body)
+
+	if json_body.Username == "" {
+		json_body.Username = "Anonymous"
+	}
+
+	user, err = io.UpdateUname(user.ID, json_body.Username)
+
+	if err != nil {
+		HandleHTTPErr(response, err, 500)
+		return
+	}
+
+	response_map = map[string]interface{} {
+		"username": user.Name,
+	}
+
+	SendHTTPJsonResponse(response, response_map)
+	return
+}
+
 func postHandleUser(response http.ResponseWriter, request *http.Request) {
 	var body []byte
-	var body_json struct {
+	var json_body struct {
 		Username string `json:"username"`
 		Password string `json:"password"`
 	}
@@ -28,24 +87,20 @@ func postHandleUser(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	err = json.Unmarshal(body, &body_json)
+	err = json.Unmarshal(body, &json_body)
 
-	log.Printf("body_json: %s", body_json)
-
-	log.Printf("passwd: %s", body_json.Password)
-	if body_json.Password == "" {
+	if json_body.Password == "" {
 		HandleHTTPErr(response, errors.New("password_missing"), 400)
 		return
 	}
 
-	log.Printf("uname: %s", body_json.Username)
-	if body_json.Username == "" {
-		body_json.Username = "Anonymous"
+	if json_body.Username == "" {
+		json_body.Username = "Anonymous"
 	}
 
-	var user models.User = io.NewUser(body_json.Username, body_json.Password)
+	var user models.User = io.NewUser(json_body.Username, json_body.Password)
 	var key string
-	key, err = io.NewKey(user.ID, body_json.Password)
+	key, err = io.NewKey(user.ID, json_body.Password)
 
 	if err != nil {
 		HandleHTTPErr(response, err, 500)
@@ -63,5 +118,9 @@ func HandleUser(response http.ResponseWriter, request *http.Request) {
 	switch request.Method {
 	case "POST":
 		postHandleUser(response, request)
+		return
+	case "PUT":
+		putHandleUser(response, request)
+		return
 	}
 }
--- server/user_test.go
+++ server/user_test.go
@@ -5,7 +5,10 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"fmt"
     "encoding/json"
+	"chat/io"
+	"chat/models"
 )
 
 func postUserTest(test *testing.T, data string) {
@@ -41,6 +44,34 @@ func postUserTest(test *testing.T, data string) {
     }
 }
 
+func putUserTest(test *testing.T, data string, key string) {
+	var post_data string = data
+	var handler http.HandlerFunc = http.HandlerFunc(HandleUser)
+	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
+
+	var request *http.Request
+	var err error
+	request, err = http.NewRequest("PUT", fmt.Sprintf("/user?key=%s", key), strings.NewReader(post_data))
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	handler.ServeHTTP(recorder, request)
+    var response struct{
+        Username string `json:"username"`
+    }
+    json.Unmarshal([]byte(recorder.Body.String()), &response)
+
+    if response.Username == "" {
+        test.Errorf("username got: %s", response.Username)
+    }
+
+    if recorder.Code != 200 {
+        test.Errorf("response.Code expected: 200, got: %d", recorder.Code)
+        test.Errorf("response: %s", recorder.Body.String())
+    }
+}
+
 func Test_postHandleUser(test *testing.T) {
     postUserTest(test, "{\"username\":\"foobar\",\"password\":\"foobar2000\"}")
 }
@@ -51,4 +82,37 @@ func Test_postHandleUserNoUname(test *testing.T) {
 
 func Test_postHandleUserBadUname(test *testing.T) {
     postUserTest(test, "{\"username\":42,\"password\":\"foobar2000\"}")
+}
+
+func Test_putNewUsername(test *testing.T) {
+	var key string
+	var err error
+	var exists bool
+	var user models.User = io.NewUser("foobar", "foobar2000")
+
+	if user.Name != "foobar" {
+		test.Errorf("user.Name expected: foobar, got: %s", user.Name)
+	}
+
+	key, err = io.NewKey(user.ID, "foobar2000")
+
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	putUserTest(test, "{\"username\":\"foobar3000\"}", key)
+
+	user, exists, err = io.UserFromKey(key)
+
+	if !exists {
+		test.Fatalf("User with key %s does not exist", key)
+	}
+
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	if user.Name != "foobar3000" {
+		test.Errorf("user.Name expected: foobar3000, got: %s", user.Name)
+	}
 }
