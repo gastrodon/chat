87f5ec2e05ae95e1bab04d5e302da51585b15e85
Zero <dakoolstwunn@gmail.com>
2019-10-20 00:44:40 -0400

    DELETE /key

--- server/key_test.go
+++ server/key_test.go
@@ -3,7 +3,6 @@ package server
 import (
 	"chat/io"
 	"chat/models"
-	"chat/util"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -12,11 +11,30 @@ import (
 	"testing"
 )
 
+func HTTPTestRequest(method string, endpoint string, post_data *string, handler_func func(http.ResponseWriter, *http.Request)) (*httptest.ResponseRecorder, error) {
+	var recorder *httptest.ResponseRecorder = httptest.NewRecorder()
+	var request *http.Request
+	var err error
+	if post_data != nil {
+		request, err = http.NewRequest(method, endpoint, strings.NewReader(*post_data))
+	} else {
+		request, err = http.NewRequest(method, endpoint, nil)
+	}
+
+	if err != nil {
+		return recorder, err
+	}
+
+	var handler http.HandlerFunc = http.HandlerFunc(handler_func)
+	handler.ServeHTTP(recorder, request)
+	return recorder, nil
+}
+
 func postKeyTest(test *testing.T, user_id string, password string) {
 	var post_data string = fmt.Sprintf("{\"user_id\":\"%s\",\"password\":\"%s\"}", user_id, password)
 	var recorder *httptest.ResponseRecorder
 	var err error
-	recorder, err = util.HTTPTestRequest("POST", "/key", &post_data, HandleKey)
+	recorder, err = HTTPTestRequest("POST", "/key", &post_data, HandleKey)
 
 	if err != nil {
 		test.Fatal(err)
@@ -53,6 +71,51 @@ func postKeyTest(test *testing.T, user_id string, password string) {
 	}
 }
 
+func deleteKeyTest(test *testing.T, user_id string, key string) {
+	var post_data string = fmt.Sprintf("{\"user_id\":\"%s\",\"key\":\"%s\"}", user_id, key)
+	var recorder *httptest.ResponseRecorder
+	var err error
+	recorder, err = HTTPTestRequest("DELETE", "/key", &post_data, HandleKey)
+
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	var response struct {
+		UserID string `json:"user_id"`
+	}
+	json.Unmarshal([]byte(recorder.Body.String()), &response)
+
+	if response.UserID == "" {
+		test.Error("No user_id returned")
+	}
+
+	if response.UserID != user_id {
+		test.Errorf("user_id expected: %s, got: %s", user_id, response.UserID)
+	}
+
+	var exists bool
+	_, exists, err = io.UserFromKey(key)
+
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	if exists {
+		test.Error("UserFromKey key does exist")
+	}
+
+	_, exists, err = io.UserFromID(user_id)
+
+	if err != nil{
+		test.Fatal(err)
+	}
+
+	if !exists {
+		test.Error("UserFromID user_id does not exist")
+	}
+}
+
 func errKeyTest(test *testing.T, method string, data string, error_desc string, code int) {
 	var post_data string = data
 	var handler http.HandlerFunc = http.HandlerFunc(HandleKey)
@@ -78,15 +141,17 @@ func errKeyTest(test *testing.T, method string, data string, error_desc string, 
 
 	if recorder.Code != code {
 		test.Errorf("response.Code expected: %d, got: %d", code, recorder.Code)
+		test.Errorf("response: %s", recorder.Body.String())
 	}
 }
 
 func Test_postHandleKey(test *testing.T) {
+	var uname string = "foobar"
 	var password string = "foobar2000"
-	var user models.User = io.NewUser("foobar", password)
+	var user models.User = io.NewUser(uname, password)
 
-	if user.Name != "foobar" {
-		test.Fatalf("user.Name expected: foobar, got: %s", user.Name)
+	if user.Name != uname {
+		test.Fatalf("user.Name expected: %s got: %s", uname, user.Name)
 	}
 
 	postKeyTest(test, user.ID, password)
@@ -133,6 +198,58 @@ func Test_postHandleKeyMalformed(test *testing.T) {
 	var post_data string = "Wait, this isn't json!{{{{{{]]][:]}"
 
 	errKeyTest(test, "POST", post_data, "malformed_json", 400)
+}
+
+func Test_deleteHandleKey(test *testing.T) {
+	var uname string = "foobar"
+	var password string = "foobar2000"
+	var user models.User = io.NewUser(uname, password)
+
+	if user.Name != uname {
+		test.Fatalf("user.Name expected: %s got: %s", uname, user.Name)
+	}
+
+	var key string
+	var err error
+	key, err = io.NewKey(user.ID, password)
+
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	deleteKeyTest(test, user.ID, key)
+}
+
+func Test_deleteHandleKeyMissingKey(test *testing.T) {
+	var post_data string = "{\"user_id\":\"0\"}"
+
+	errKeyTest(test, "DELETE", post_data, "key_missing", 400)
+}
+
+func Test_deleteHandleKeyMissingUserID(test *testing.T) {
+	var post_data string = "{\"key\":\"0\"}"
+
+	errKeyTest(test, "DELETE", post_data, "user_id_missing", 400)
+}
+
+func Test_deleteHandleKeyNoSuchUser(test *testing.T) {
+	var post_data string = "{\"key\":\"0\",\"user_id\":\"0\"}"
+
+	errKeyTest(test, "DELETE", post_data, "bad_key", 401)
+}
+
+func Test_deleteHandleKeyBadKey(test *testing.T) {
+	var uname string = "foobar"
+	var password string = "foobar2000"
+	var user models.User = io.NewUser(uname, password)
+
+	if user.Name != uname {
+		test.Fatalf("user.Name expected: %s got: %s", uname, user.Name)
+	}
+
+	var post_data string = fmt.Sprintf("{\"key\":\"0\",\"user_id\":\"%s\"}", user.ID)
+
+	errKeyTest(test, "DELETE", post_data, "bad_key", 401)
 }
 
 func Test_HandleKeyBadMethod(test *testing.T) {
